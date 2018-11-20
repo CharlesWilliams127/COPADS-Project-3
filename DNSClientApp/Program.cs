62ddfdff5746a6f456aadecef4c6c7db537a4ceb
Charles Williams (RIT Student) <crw4147@ad.rit.edu>
2018-11-19 19:32:16 -0500

    Successfully Parsing CNAME Pointers

--- DNSClientApp/Program.cs
+++ DNSClientApp/Program.cs
@@ -11,16 +11,18 @@ namespace DNSClientApp
 {
     class Program
     {
-        const string DEFUALT_DNS = "8.8.8.8";
+        const string DEFUALT_DNS = "129.21.3.17";
         const int ANSWER_RR_INDEX = 6;
+        const byte REFERENCE_BYTE = 0xc0;
+        const byte PERIOD_BYTE = 0x2e;
 
         public static void Main(string[] args)
         {
             if (args.Length == 1)
             {
                 var host = args[0];
-                var p1 = new Program(); 
-                p1.getData(host, p1.prepareQuery("www.snapchat.com", "A"));
+                var p1 = new Program();
+                p1.getData(host, p1.prepareQuery("www.snapchat.com", "AAAA"));
                 Console.ReadKey();
             }
             else
@@ -28,6 +30,8 @@ namespace DNSClientApp
                 System.Console.WriteLine("Usage: dotnet run <host>");
             }
         }
+
+
 
         public byte[] prepareQuery(string requestText, string typeString)
         {
@@ -40,7 +44,7 @@ namespace DNSClientApp
 
             byte[] type = new byte[2];
             // determine type
-            if(typeString == "AAAA")
+            if (typeString == "AAAA")
             {
                 type[0] = 0x00;
                 type[1] = 0x1c;
@@ -68,7 +72,7 @@ namespace DNSClientApp
             int index = startIndex;
             while (index < queryList.Count)
             {
-                if (queryList[index] == 0x2e) // period
+                if (queryList[index] == PERIOD_BYTE) // period
                 {
                     byte[] stringCount = BitConverter.GetBytes(index - (startIndex + 1)).TakeWhile(b => { return b != 0x00; }).ToArray();
                     if (BitConverter.IsLittleEndian) Array.Reverse(stringCount);
@@ -94,11 +98,25 @@ namespace DNSClientApp
             return queryList.ToArray();
         }
 
+        public static List<byte> parsePointer(List<byte> operateList, List<byte> initList,int index, int count)
+        {
+            operateList.AddRange(initList.GetRange(index + 1, count));
+
+            if (initList[index + count + 1] == 0x00)
+            {
+                return operateList;
+            }
+            else // also signifies a period should be added
+            {
+                operateList.Add(PERIOD_BYTE);
+                return parsePointer(operateList, initList, index + count + 1, initList[index + count + 1]);
+            }
+        }
+
         public async void getData(string host, byte[] query)
         {
             try
             {
-                //var sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 var client = new UdpClient();
 
                 IPAddress serverAddr = IPAddress.Parse(DEFUALT_DNS);
@@ -116,15 +134,59 @@ namespace DNSClientApp
                 // find out how many responses there are
                 var resultList = result.Buffer.ToList();
                 int responseNum = (resultList[ANSWER_RR_INDEX] << 8) | resultList[ANSWER_RR_INDEX + 1];
-
-                // begin to loop through responses
-                for(int i = 0; i < responseNum; i++)
-                {
-
-                }
                 var answerStartIndex = query.Count();
 
-                var val = resultList[answerStartIndex];
+                // index to keep track of where we are in the stream
+                int index = answerStartIndex;
+
+                // create lists to store full parsed answers
+                List<byte>[] parsedResponses = new List<byte>[responseNum];
+                for (int i = 0; i < responseNum; i++)
+                {
+                    parsedResponses[i] = new List<byte>();
+                }
+
+                // bool to kick us out of the loop when one of our responses has ended
+                bool responseParsed = false;
+
+                // begin to loop through answer section responses
+                for (int i = 0; i < responseNum; i++)
+                {
+                    responseParsed = false;
+                    while (!responseParsed)
+                    {
+                        // check if data is a pointer
+                        if (resultList[index] == REFERENCE_BYTE)
+                        {
+                            // our next byte is the offset into the stream
+                            // first byte into the offset will be the count
+                            // create a sub for loop to parse string pointed at
+                            int pointerIndex = (int)resultList[index + 1];
+
+                            // read in string from pointed at bit
+                            int pointerStringLength = (int)resultList[pointerIndex];
+
+                            // add in this range to our parsed responses
+                            //for(int j = pointerIndex; i < pointerStringLength; i+=)
+                            //parsedResponses[i].AddRange(resultList.GetRange(pointerIndex, pointerStringLength));
+                            parsedResponses[i] = parsePointer(parsedResponses[i], resultList, pointerIndex, pointerStringLength);
+                        }
+
+                        // reached the beginning of the length section for actual data
+                        if (index == answerStartIndex + 10)
+                        {
+                            // this byte and the next will be the length of the data section
+                            int dataStringLength = (resultList[index] << 8 | resultList[index + 1]);
+
+                            parsedResponses[i].AddRange(resultList.GetRange(index + 2, dataStringLength));
+
+                            // we've read this data and added it to the appropriate array
+                            // mark this data as parsed
+                            responseParsed = true;
+                        }
+                        index++;
+                    }
+                }
             }
             catch (Exception e)
             {
